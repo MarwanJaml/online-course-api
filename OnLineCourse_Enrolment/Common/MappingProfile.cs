3a8cfcd2a734bb72ec00a2799fe3b6cc447f3795
Marwan Jaml <marwanaljaml11@gmail.com>
2025-06-21 23:23:25 +0300

    Add videoRequest and other features

---
+++ OnLineCourse_Enrolment/Common/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OnLineCourse.Core.Entities;
+using OnLineCourse.Core.Models;
+
+namespace OnLineCourse_Enrolment.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<VideoRequest, VideoRequestModel>()
+             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName}, {src.User.LastName}"));
+
+            CreateMap<VideoRequestModel, VideoRequest>()
+                .ForMember(dest => dest.User, opt => opt.Ignore()); // We don't map User here since it's handled separately
+        }
+    }
+}
